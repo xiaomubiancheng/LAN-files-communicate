6611bafd95627eda5387739af1dbd989e60af3f0
xiaomubiancheng <1462906364@qq.com>
2022-03-11 15:41:54 +0800

    gin && cmd init

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module deskTool
 
 go 1.16
 
-require github.com/zserge/lorca v0.1.10 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/zserge/lorca v0.1.10 // indirect
+)
--- main.go
+++ main.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"os"
+	"os/exec"
 	"os/signal"
-	"syscall"
 
-	"github.com/zserge/lorca"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	var ui lorca.UI
-	ui, _ = lorca.New("https://baidu.com", "", 800, 600, "--disable-sync", "--disable-translate")
-	chSignal := make(chan os.Signal, 1)
-	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
-	select {
-	case <-ui.Done():
-	case <-chSignal:
-	}
 
-	ui.Close()
+	go func() {
+		r := gin.Default()
+		r.GET("/", func(c *gin.Context) {
+			c.Writer.Write([]byte("test"))
+		})
+		r.Run()
+	}()
+
+	chromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
+	cmd.Start()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	select {
+	case <-quit:
+		cmd.Process.Kill()
+	}
 
 }
