659612421ae2e88a7ffa21eeeaec9dcdeb67840c
xiaomubiancheng <1462906364@qq.com>
2022-03-11 22:54:55 +0800

    基础接口完成

--- main.go
+++ main.go
@@ -1,33 +1,194 @@
 package main
 
 import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/skip2/go-qrcode"
 )
+
+var FS embed.FS
 
 func main() {
 
 	go func() {
 		r := gin.Default()
-		r.GET("/", func(c *gin.Context) {
-			c.Writer.Write([]byte("test"))
+
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+
+		fmt.Println("sddd", staticFiles)
+
+		r.StaticFS("/static", http.FS(staticFiles))
+
+		//
+		r.POST("/api/v1/files", FilesController)
+		r.GET("/api/v1/qrcodes", QrcodesController)
+		r.GET("/upload/:path", UploadsController)
+		r.GET("/api/v1/address", AddressesController)
+		r.POST("/api/v1/texts", TextController)
+		//
+
+		r.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+
+			fmt.Println("path=", path)
+			if strings.HasPrefix(path, "/static/") {
+
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if err != nil {
+					fmt.Println("open file err=", err)
+					return
+				}
+
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
 		})
+
 		r.Run()
 	}()
 
 	chromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index.html")
 	cmd.Start()
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 
 	select {
+	//监听Ctrl+c
 	case <-quit:
 		cmd.Process.Kill()
 	}
 
+}
+
+func FilesController(c *gin.Context) {
+	file, err := c.FormFile("raw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := uuid.New().String()
+	uploads := filepath.Join(dir, "uploads")
+	err = os.MkdirAll(uploads, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
+	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
+}
+
+func QrcodesController(c *gin.Context) {
+	if content := c.Query("content"); content != "" {
+		png, err := qrcode.Encode(content, qrcode.Medium, 256)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.Data(http.StatusOK, "image/png", png)
+	} else {
+		c.Status(http.StatusBadRequest)
+	}
+}
+
+func GetUploadsDir() (uploads string) {
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	uploads = filepath.Join(dir, "uploads")
+	return
+}
+
+func UploadsController(c *gin.Context) {
+	if path := c.Param("path"); path != "" {
+		target := filepath.Join(GetUploadsDir(), path)
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Transfer-Encoding", "binary")
+		c.Header("Content-Disposition", "attachment; filename="+path)
+		c.Header("Content-Type", "application/octet-stream")
+		c.File(target)
+	} else {
+		c.Status(http.StatusNotFound)
+	}
+}
+
+// 获取电脑在各个局域网的ip地址
+func AddressesController(c *gin.Context) {
+	addrs, _ := net.InterfaceAddrs()
+	var result []string
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				result = append(result, ipnet.IP.String())
+			}
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"addresses": result})
+}
+
+func TextController(c *gin.Context) {
+	var json struct {
+		Raw string `json:"raw"`
+	}
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		exe, err := os.Executable() // 获取当前执行文件的路径
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Dir(exe) // 当前执行文件的目录
+
+		filename := uuid.New().String()          // 生成文件名
+		uploads := filepath.Join(dir, "uploads") // 拼接uploads的绝对路径
+		err = os.MkdirAll(uploads, os.ModePerm)  //创建uploads目录
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullpath := path.Join("uploads", filename+".txt") //文件的路径
+		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath}) // 返回文件的绝对路径
+
+	}
 }
